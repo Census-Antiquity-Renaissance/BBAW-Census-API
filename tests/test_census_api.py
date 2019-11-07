38430466f56c805bded76758576a913e006ec2d4
opohl <opohl@bbaw.de>
2019-11-07 14:34:30 +0100

    Revert "Initial commit"
    This reverts commit 5fbadbd63a2e92820df6713bda48b5c7763aaec8.

--- tests/test_census_api.py
+++
@@ -1,9 +0,0 @@
-from census_api.main import app
-from starlette.testclient import TestClient
-
-client = TestClient(app)
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
